3e836cafee7e90065e98031b24b48b84712f36c0
Md Faizan <mfrnmdfaizan@gmail.com>
2026-07-13 10:16:18 +0530

    Fix formatting issues in sumofarr.cpp

--- DSA Summer Training (C++)  2026/DSA  (C++)/sumofarr.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/sumofarr.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
     int arr[] = {10, 20, 30, 40,50};
     int size = sizeof(arr) / sizeof(arr[0]);
-
     int sum = 0;
     for (int i = 0; i < size; i++) {
         sum += arr[i];
