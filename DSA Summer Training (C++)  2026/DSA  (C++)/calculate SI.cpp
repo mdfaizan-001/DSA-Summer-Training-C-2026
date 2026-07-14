b87823abdb714b24c8b8323c2b7475b40bfbf8a2
Md Faizan <mfrnmdfaizan@gmail.com>
2026-07-14 10:27:05 +0530

    Refactor calculate SI.cpp for cleaner formatting

--- DSA Summer Training (C++)  2026/DSA  (C++)/calculate SI.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/calculate SI.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+    
     float principal, rate, time, simpleInterest;
-
     cout << "Enter principal amount: ";
     cin >> principal;
     cout << "Enter rate of interest: ";
     cin >> rate;
-
     cout << "Enter time period: ";
     cin >> time;
     simpleInterest = (principal * rate * time) / 100;
-
     cout << "Simple Interest = " << simpleInterest;
 
     return 0;
