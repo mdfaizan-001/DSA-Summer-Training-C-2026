334e484849f7e8b94483224afba675a523c4605c
Md Faizan <mfrnmdfaizan@gmail.com>
2026-07-14 10:27:45 +0530

    Refactor calculate percentage program formatting

--- DSA Summer Training (C++)  2026/DSA  (C++)/calucate Per.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/calucate Per.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using namespace std;
-
 int main () {
+    
     float marksObtained, totalMarks, Percentage;
-
     cout << "Enter obtained marks: ";
     cin >> marksObtained;
-
     cout << "Enter total marks: ";
     cin >> totalMarks;
-
     Percentage = (marksObtained / totalMarks)*100;
-
     cout << "Percentage = " << Percentage << "%";
 
     return 0;
