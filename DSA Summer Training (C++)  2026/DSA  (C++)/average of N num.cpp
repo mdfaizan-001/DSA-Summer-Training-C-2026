57333a4188b7d9b05b504c8ad5ccc588984e11ef
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-03 23:19:06 +0530

    Fix formatting issues in average of N num.cpp

--- DSA Summer Training (C++)  2026/DSA  (C++)/average of N num.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/average of N num.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     int n;
     float num, sum = 0, average;
     cout << "Enter the number of values: ";
     cin >> n;
-
     for (int i = 0; i < n; i++) {
         cout << "Enter number " << i << ": ";
         cin >> num;
