9ad4d3481d1d3f42d9efbd7fc524aa9c7c74fa27
Md Faizan <mfrnmdfaizan@gmail.com>
2026-08-03 23:17:21 +0530

    Refactor check even and odd program structure

--- DSA Summer Training (C++)  2026/DSA  (C++)/check even and odd.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/check even and odd.cpp
@@ -3,14 +3,11 @@ using namespace std;
 
 int main() {
     int num;
-
     cout << "Enter a number: ";
     cin >> num;
-
     if (num % 2 == 0)
         cout << num << " is an even ";
     else
         cout << num << " is an odd ";
-
     return 0;
 }
