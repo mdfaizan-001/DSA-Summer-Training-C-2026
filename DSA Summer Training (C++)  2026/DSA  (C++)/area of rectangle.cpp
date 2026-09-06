adfcdd6a1efe017ccd8d429a33c45dcaae565ab1
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-06 19:14:03 +0530

    Refactor area of rectangle program formatting

--- DSA Summer Training (C++)  2026/DSA  (C++)/area of rectangle.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/area of rectangle.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     float length,breadth,area;
     cout << "Enter the Length: ";
     cin >> length;
-
     cout << "Enter the breadth: ";
     cin >> breadth;
-
     area = length * breadth;
-    cout << "Area of rectangle is = " << area;
-    
+    cout << "Area of rectangle is = " << area; 
     return 0;
 }
