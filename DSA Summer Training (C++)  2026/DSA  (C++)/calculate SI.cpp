57dd4d40632e76b3865d09e8c87a052f9a711a6f
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-06 19:16:21 +0530

    Update calculate SI.cpp

--- DSA Summer Training (C++)  2026/DSA  (C++)/calculate SI.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/calculate SI.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-int main() {
-    
+int main() {  
     float principal, rate, time, simpleInterest;
     cout << "Enter principal amount: ";
     cin >> principal;
@@ -11,6 +10,5 @@ int main() {
     cin >> time;
     simpleInterest = (principal * rate * time) / 100;
     cout << "Simple Interest = " << simpleInterest;
-
     return 0;
 }
