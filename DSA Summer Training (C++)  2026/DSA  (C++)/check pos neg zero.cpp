eaa3d2ee50de8f6059531ff3df4c909d7c4e61cc
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-08 19:11:28 +0530

    Update check pos neg zero.cpp

--- DSA Summer Training (C++)  2026/DSA  (C++)/check pos neg zero.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/check pos neg zero.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int num;
-
     cout << "Enter a number: ";
     cin >> num;
-    
     if (num > 0)
         cout << "Positive number";
     else if (num < 0)
