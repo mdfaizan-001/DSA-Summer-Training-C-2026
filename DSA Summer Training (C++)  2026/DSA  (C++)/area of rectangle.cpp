78a1f92a36a6603e2f8a30f385a2308b331c0f7e
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-10 21:02:42 +0530

    Update area of rectangle.cpp

--- DSA Summer Training (C++)  2026/DSA  (C++)/area of rectangle.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/area of rectangle.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
     float length,breadth,area;
     cout << "Enter the Length: ";
     cin >> length;
@@ -9,6 +8,5 @@ int main() {
     cin >> breadth;
     area = length * breadth;
     cout << "Area of rectangle is = " << area; 
-    return 0;
-    
+    return 0;  
 }
