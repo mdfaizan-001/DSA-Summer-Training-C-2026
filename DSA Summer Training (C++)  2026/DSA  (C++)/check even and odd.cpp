72eafb707632f76b307083b288c43aac7bf8dd8f
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-14 18:04:11 +0530

    Format check even and odd.cpp for consistency

--- DSA Summer Training (C++)  2026/DSA  (C++)/check even and odd.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/check even and odd.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 int main() {
+    
     int num;
     cout << "Enter a number: ";
     cin >> num;
+    
     if (num % 2 == 0)
         cout << num << " is an even ";
     else
         cout << num << " is an odd ";
     return 0;
+    
 }
