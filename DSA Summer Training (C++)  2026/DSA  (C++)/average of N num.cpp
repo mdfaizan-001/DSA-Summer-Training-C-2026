2513e2a13f0ab7d6d61d2f808fa318ef79a6caa9
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-16 19:05:42 +0530

    Update average of N num.cpp

--- DSA Summer Training (C++)  2026/DSA  (C++)/average of N num.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/average of N num.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-int main() {
-    
+int main() {    
     int n;
     float num, sum = 0, average;
     cout << "Enter the number of values: ";
@@ -10,8 +9,7 @@ int main() {
         cout << "Enter number " << i << ": ";
         cin >> num;
         sum += num;
-    }
-    
+    }   
     average = sum / n;
     cout << "Average: " << average;
     return 0;
