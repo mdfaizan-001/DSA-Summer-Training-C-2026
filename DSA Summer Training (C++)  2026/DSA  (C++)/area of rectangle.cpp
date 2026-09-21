955c970f5ba4f1c6d348faedc705947bc4132f31
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-21 21:03:08 +0530

    Format area of rectangle.cpp for consistency

--- DSA Summer Training (C++)  2026/DSA  (C++)/area of rectangle.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/area of rectangle.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
+
 int main() {    
+    
     float length,breadth,area;
     cout << "Enter the Length: ";
     cin >> length;
     cout << "Enter the breadth: ";
     cin >> breadth;
     area = length * breadth;
+    
     cout << "Area of rectangle is = " << area; 
-    return 0;     
+    return 0;
+    
 }
