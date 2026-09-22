59318f4bf80e8950947155e67267a5da89f26e91
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-22 21:33:38 +0530

    Remove unnecessary lines from largest element program

--- DSA Summer Training (C++)  2026/DSA  (C++)/largestelementofarr.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/largestelementofarr.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using namespace std;
-
-int main() {  
-    
+int main() {      
     int arr[5] = {12, 45, 7, 23, 89};
     int largest = arr[0];
     for (int i = 1; i < 5; i++) {
         if (arr[i] > largest) {
             largest = arr[i];
         }
-    } 
-    
+    }  
     cout << "Largest element: " << largest;
-    return 0;
-    
+    return 0;    
 }
