854e88afd2186a14f55848051fe4f83fbc3620bb
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-22 21:09:14 +0530

    Refactor search element in array program

--- DSA Summer Training (C++)  2026/DSA  (C++)/search element in array.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/search element in array.cpp
@@ -1,31 +1,23 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    
+int main() {  
     int arr[5], search, found =0;
     cout << "Enter 5 num:\n";
     for (int i = 0; i < 5; i++) {
         cin >> arr[i];
-    }
-    
+    }  
     cout << "Enter the number to search: ";
-    cin >> search;
-    
+    cin >> search;  
     for (int i = 0; i < 5; i++) {
         if (arr[i] == search) {
             found = 1;
             break;
-        } 
-        
-    }
-    
+        }     
+    }  
     if (found) {
         cout << "Element found." << endl;
     } else {
         cout << "Element not found." << endl;
-    }
-    
-    return 0;
-    
+    }  
+    return 0;   
 }
