469af30d4d2eeab560a89e9df860e42b1d00dcf7
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-22 21:48:32 +0530

    Refactor largest of two numbers program

--- DSA Summer Training (C++)  2026/DSA  (C++)/largest of two num.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/largest of two num.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() {
-    
+int main() {    
     int num1, num2;
     cout << "Enter first number: ";
     cin >> num1;
     cout << "Enter second number: ";
-    cin >> num2;
-    
+    cin >> num2;   
     if (num1 > num2)
-        cout << "Largest number = " << num1;
-        
+        cout << "Largest number = " << num1;       
     else if (num2 > num1)
-        cout << "Largest number = " << num2;
-        
+        cout << "Largest number = " << num2;        
     else
         cout << "Both numbers are equal";
-    return 0;
-    
+    return 0;    
 }
