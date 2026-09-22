a251ec52342aff3b8075e94d9a0761c1145637f3
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-22 21:05:23 +0530

    Update largestelementofarr.cpp

--- DSA Summer Training (C++)  2026/DSA  (C++)/largestelementofarr.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/largestelementofarr.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int arr[5] = {12, 45, 7, 23, 89};
-
     int largest = arr[0];
-
     for (int i = 1; i < 5; i++) {
         if (arr[i] > largest) {
             largest = arr[i];
