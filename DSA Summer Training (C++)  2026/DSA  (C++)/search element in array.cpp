b3f666efee184339a19703d39bc84c69b9089518
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-23 21:07:05 +0530

    Update search element in array.cpp

--- DSA Summer Training (C++)  2026/DSA  (C++)/search element in array.cpp
+++ DSA Summer Training (C++)  2026/DSA  (C++)/search element in array.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
-int main() {  
+
+int main() { 
+    
     int arr[5], search, found =0;
     cout << "Enter 5 num:\n";
     for (int i = 0; i < 5; i++) {
         cin >> arr[i];
-    }  
+    } 
+    
     cout << "Enter the number to search: ";
     cin >> search;  
     for (int i = 0; i < 5; i++) {
@@ -14,10 +17,14 @@ int main() {
             break;
         }     
     }  
+    
     if (found) {
+        
         cout << "Element found." << endl;
     } else {
         cout << "Element not found." << endl;
-    }  
-    return 0;   
+    } 
+    
+    return 0;  
+    
 }
